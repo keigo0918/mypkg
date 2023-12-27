1e447d2e4e1add055ef7fcd35347af0c787d63d8
KIG <147677592+keigo0918@users.noreply.github.com>
2023-12-27 21:43:59 +0900

    Update talk_listen.launch.py

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -6,9 +6,7 @@ import launch.actions
 import launch.substitutions
 import launch_ros.actions
 
-
 def generate_launch_description():
-
     talker = launch_ros.actions.Node(
             package='mypkg',
             executable='talker',
