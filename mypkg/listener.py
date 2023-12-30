66d17ba3e1c0efd3ed5f63aa071d987e4368ae85
KIG <147677592+keigo0918@users.noreply.github.com>
2023-12-30 19:31:04 +0900

    Update listener.py

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,19 +1,12 @@
-# SPDX-FileCopyrightText: 2023 Keigo Shishido
-# SPDX-License-Identifier: BSD-3-Clause
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
-class Listener():
-    def __init__(self, node):
-        self.node = node
-        self.pub = node.create_subscription(Int16, "countup", self.cb, 10)
+def cb(msg):
+    global node
+    node.get_logger().info("Listen: %d" % msg.data)
 
-    def cb(self, msg):
-        self.node.get_logger().info("Listen: %d" % msg.data)
-
-def main():
-    rclpy.init()
-    node = Node("listener")
-    listener = Listener(node)
-    rclpy.spin(node)
+rclpy.init()
+node = Node("listener")
+pub = node.create_subscription(Int16, "countup", cb, 10)
+rclpy.spin(node)
