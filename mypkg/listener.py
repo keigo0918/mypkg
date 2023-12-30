34ad7be4fb3448858b02604be8a969728289e601
KIG <147677592+keigo0918@users.noreply.github.com>
2023-12-30 19:39:42 +0900

    Update listener.py

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,19 +1,31 @@
-# SPDX-FileCopyrightText: 2023 Keigo Shishido
-# SPDX-License-Identifier: BSD-3-Clause
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
-class Listener():
-    def __init__(self, node):
-        self.node = node
-        self.pub = node.create_subscription(Int16, "countup", self.cb, 10)
-
-    def cb(self, msg):
-        self.node.get_logger().info("Listen: %d" % msg.data)
-
 def main():
     rclpy.init()
     node = Node("listener")
-    listener = Listener(node)
-    rclpy.spin(node)
+    client = node.create_client(Query, 'query')
+    while not client.wait_for_service(timeout_sec=0.5):
+        node.get_logger().info('待機中')
+
+    req = Query.Request()
+    req.name = "上田隆一"
+    future = client.call_async(req)
+    while rclpy.ok():
+        rclpy.spin_once(node)
+        if future.done():
+            try:
+                response = future.result()
+            except:
+                node.get_logger().info('')
+            else:
+                node.get_logger().info("age: {}".format(response.age))
+
+            break
+
+    node.destroy_node()
+    rclpy.shutdown()
+    
+if __name__ == '__main__':
+    main()
